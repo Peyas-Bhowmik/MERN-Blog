d1b0ffb61e6f7fcd0470d9432a88a951e1fad400
Peyas-Bhowmik <87723126+Peyas-Bhowmik@users.noreply.github.com>
2021-07-23 00:09:46 +0600

    update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = connect = async () => {
     try {
-        const response = await mongoose.connect(CONNECTION_URL,{useUnifiedTopology: true,useNewUrlParser: true,useFindAndModify: false,})
+        const response = await mongoose.connect(process.env.CONNECTION_URL,{useUnifiedTopology: true,useNewUrlParser: true,useFindAndModify: false,})
         console.log('success');
     } catch (error) {
         console.log(`no success ${error}`);
