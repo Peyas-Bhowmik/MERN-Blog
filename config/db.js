eb1d218e41ad7bd9c19eea1b1cc909f224ce51b6
Peyas-Bhowmik <rboy4046@gmail.com>
2021-07-22 23:11:31 +0600

    save changes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = connect = async () => {
     try {
-        const response = await mongoose.connect(process.env.CONNECTION_URL,{useUnifiedTopology: true,useNewUrlParser: true,useFindAndModify: false,})
+        const response = await mongoose.connect("mongodb+srv://blogapp:blogapp1234@cluster1.igmwu.mongodb.net/blog?retryWrites=true&w=majority",{useUnifiedTopology: true,useNewUrlParser: true,useFindAndModify: false,})
         console.log('success');
     } catch (error) {
         console.log(`no success ${error}`);
